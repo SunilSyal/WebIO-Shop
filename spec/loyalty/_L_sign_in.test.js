7e060afcb26f804c0ffadbc942c0f827c422604d
SunilSyal <sunil.syal@gmail.com>
2016-11-03 21:18:56 +0000

    Updated Loyalty test cases

--- spec/loyalty/_L_sign_in.test.js
+++ spec/loyalty/_L_sign_in.test.js
@@ -1,61 +1,52 @@
 describe('Sparks Sign In Journey', function() {
 
- it('Should have a title', function() {
-        browser.url('http://.marksandspencer.com');
-        expect(browser.getTitle()).to.contain('Welcome to Marks');
-    });
+    beforeEach(function() {
+        browser.url('http://marksandspencer.com');
+    })
 
-    it('Verify sparks link and clik should take to sparks page', function() {
-        browser.url('http://.marksandspencer.com');
+    function sparksPageVerification() {
+        browser.waitForVisible('.sparks a.link');
         browser.click('.sparks a.link');
         expect(browser.getTitle()).to.contain('M&S Sparks');
+    }
 
-    });
-
-    it('Verify Charity page', function() {
-      this.timeout(0);
-      browser.url('http://.marksandspencer.com');
-      browser.waitForVisible('.sparks a.link',5000);
-      browser.click('.sparks a.link');
-      expect(browser.getTitle()).to.contain('M&S Sparks');
-      //Sign In to Sparks
-      browser.waitForEnabled('.btn--north-primary', 5000);
-      browser.click('.btn--north-primary');
-      //Entering Login details
-      browser.setValue('#email', 'aroraneha.16@gmail.com');
-      browser.setValue('#password', 'wcs12345');
-      //Clicking Submit button
-      browser.click('.btn--north-primary');
-      browser.waitForVisible('.sticky-element .topbar__left-wrapper', 5000);
-      expect(browser.getTitle()).to.contain('Home');
-      //expect(browser.getText('.home--error.ng-scope')).to.contain('WELCOME');
-      expect(browser.getText('div > .large-banner--faded-full')).to.contain('SPARKS');
-      browser.click('.sticky-element .topbar__left-wrapper');
-      browser.waitForVisible('div[ui-view="contentView@"] .account__home-date',5000);
-      expect(browser.getText('div[ui-view="contentView@"] .account__home-date')).to.contain('JOINED');
-      browser.moveToObject('div[ui-view] .account--home-progress');
-      browser.click('button[href="#/account/charity"]');
-      browser.waitForVisible('div[ui-view] .account__charity--details-heading')
-      expect(browser.getText('.row .account__charity--details-heading')).to.contain('CHARITY');
-
-});
-    it('Verify Sign In to sparks', function() {
-        browser.url('http://.marksandspencer.com');
-        browser.click('.sparks a.link');
-        expect(browser.getTitle()).to.contain('M&S Sparks');
+    function sparksSignIn() {
         //Sign In to Sparks
-        browser.waitForEnabled('.btn--north-primary', 7000);
+        browser.waitForEnabled('.btn--north-primary');
         browser.click('.btn--north-primary');
+
         //Entering Login details
         browser.setValue('#email', 'aroraneha.16@gmail.com');
         browser.setValue('#password', 'wcs12345');
+
+        browser.scroll('#email');
         //Clicking Submit button
         browser.click('.btn--north-primary');
-        browser.waitForVisible('.sticky-element .topbar__left-wrapper',5000);
-        expect(browser.getTitle()).to.contain('Home');
-        //expect(browser.getText('.home--error.ng-scope')).to.contain('WELCOME');
-        expect(browser.getText('div > .large-banner--faded-full')).to.contain('SPARKS');
-      });
+    }
 
+    it('Should verify journey to test sparks link and clicking it should take to sparks page and then to verify charity page', function() {
+
+        this.timeout(0);
+        expect(browser.getTitle()).to.contain('Welcome to Marks');
+
+        sparksPageVerification();
+        sparksSignIn();
+
+        browser.waitForVisible('.sticky-element .topbar__left-wrapper');
+        expect(browser.getTitle()).to.contain('Home');
+
+        expect(browser.getText('div > .large-banner--faded-full')).to.contain('SPARKS');
+        browser.click('.sticky-element .topbar__left-wrapper');
+
+        browser.waitForVisible('div[ui-view] .account__home-date');
+        expect(browser.getText('div[ui-view] .account__home-date')).to.contain('JOINED');
+
+        browser.scroll('.large-banner--account + .container');
+        browser.click('button[href="#/account/charity"]');
+
+        browser.waitForVisible('div[ui-view] .account__charity--details-heading')
+        expect(browser.getText('.row .account__charity--details-heading')).to.contain('CHARITY');
+
+    });
 
 });
