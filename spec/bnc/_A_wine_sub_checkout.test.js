3ff4415099525f5f3f598a81b883388dd0b5fd01
Raminder kaur <rkaur10@sapient.com>
2016-11-18 14:42:12 +0000

    Webdriver E2E scenarios for Bnc

---
+++ spec/bnc/_A_wine_sub_checkout.test.js
@@ -0,0 +1,22 @@
+describe.only('Wine Sub Checkout',function(){
+
+beforeEach(function() {
+      browser.url('http://www.sit2.marksandspencer.com/');
+  })
+
+  it('Should not display guest checkout option with wine sub in basket',function(){
+    this.timeout(0);
+    browser.moveToObject('a[data-analyticsid="FoodNWine_FOOD & WINE"]');
+    browser.waitForVisible('a[href*="s/food-and-wine/wine-club"]');
+    browser.moveToObject('a[href*="s/food-and-wine/wine-club"]');
+    browser.click('a[href*="s/food-and-wine/wine-club"]');
+    expect(browser.getTitle()).contain('Wine Club');
+    browser.click('a[href*="/s/food-and-wine/wine-club/club-classic?intid=fw_wineclub_classic1"]');
+    browser.scroll('#winesubaddtobag');
+    browser.click('#winesubaddtobag');
+    browser.waitForVisible('.bagNotify');
+    browser.moveToObject('a.header-link');
+    browser.click('a.btn-checkout.primary-btn');
+    browser.waitForVisible('#guestCheckoutContButton',5000, true);
+});
+});
