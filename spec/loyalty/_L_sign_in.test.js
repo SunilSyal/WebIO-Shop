6a04c2e75e9f346a141133626227fddb239908af
shub1107 <sjain64@sapient.com>
2016-12-09 12:37:12 +0000

    Changes in loyalty scenario for locator changes

--- spec/loyalty/_L_sign_in.test.js
+++ spec/loyalty/_L_sign_in.test.js
@@ -1,29 +1,47 @@
-var loyalty = require("../../common/loyaltyFunctions")
-
 describe('Sparks Sign In Journey', function() {
 
     beforeEach(function() {
         browser.url('http://marksandspencer.com');
     })
 
+    function sparksPageVerification() {
+        browser.waitForVisible('.sparks a.link');
+        browser.click('.sparks a.link');
+        expect(browser.getTitle()).to.contain('M&S Sparks');
+    }
+
+    function sparksSignIn() {
+        //Sign In to Sparks
+        browser.waitForEnabled('.btn--north-primary');
+        browser.click('.btn--north-primary');
+
+        //Entering Login details
+        browser.setValue('#email', 'jshubham007@gmail.com');
+        browser.setValue('#password', 'testing@12345');
+
+        browser.scroll('#email');
+        //Clicking Submit button
+        browser.click('.btn--north-primary');
+    }
+
     it('Should verify journey to test sparks link and clicking it should take to sparks page and then to verify charity page', function() {
 
         this.timeout(0);
         expect(browser.getTitle()).to.contain('Welcome to Marks');
 
-        loyalty.pageVerification();
-        loyalty.signIn();
+        sparksPageVerification();
+        sparksSignIn();
 
         browser.waitForVisible('.sticky-element .topbar__left-wrapper');
         expect(browser.getTitle()).to.contain('Home');
 
-        expect(browser.getText('div > .large-banner--faded-full')).to.contain('SPARKS');
+        expect(browser.getText('.large-banner--faded')).to.contain('WELCOME');
         browser.click('.sticky-element .topbar__left-wrapper');
 
         browser.waitForVisible('div[ui-view] .account__home-date');
         expect(browser.getText('div[ui-view] .account__home-date')).to.contain('JOINED');
 
-        browser.scroll('.large-banner--account + .container');
+        //browser.scroll('.large-banner--account + .container');
         browser.click('button[href="#/account/charity"]');
 
         browser.waitForVisible('div[ui-view] .account__charity--details-heading')
